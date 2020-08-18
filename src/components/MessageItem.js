140e235bcfe61bc22e15c82b3189b52cd3d6de13
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-08-19 01:11:47 +0200

    frontend: double click message to edit it

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,41 +1,88 @@
-import React from "react";
+import React, { Component } from "react";
 import Moment from "react-moment";
 import { Link } from "react-router-dom";
 import DefaultProfileImg from "../images/default-profile-image.jpg";
+import  { useRef, useEffect } from "react";
 
-const MessageItem = ({
-  date,
-  profileImageUrl,
-  text,
-  username,
-  removeMessage,
-  isCorrectUser
-}) => (
-  <div>
-    <li className="list-group-item">
-      <img
-        src={profileImageUrl || DefaultProfileImg}
-        alt={username}
-        height="100"
-        width="100"
-        className="timeline-image"
-      />
-      <div className="message-area">
-        <Link to="/">@{username} &nbsp;</Link>
-        <span className="text-muted">
-          <Moment className="text-muted" format="Do MMM YYYY">
-            {date}
-          </Moment>
-        </span>
-        <p>{text}</p>
-        {isCorrectUser && (
-          <a className="btn btn-danger" onClick={removeMessage}>
-            Delete
-          </a>
-        )}
-      </div>
-    </li>
-  </div>
-);
 
-export default MessageItem;
+export default class MessageItem extends Component {
+  constructor(props) {
+    super(props);
+    this.wrapperRef = React.createRef();
+  
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.state = {
+      edited: false,
+      text: this.props.text
+    }
+  }
+  handleChange = e => {
+    //populate state with form data
+    this.setState({
+      text: e.target.value
+    })
+  }
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+}
+
+componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+}
+  /**
+     * Alert if clicked on outside of element
+     */
+    handleClickOutside(event) {
+      if (this.wrapperRef && !this.wrapperRef.current.contains(event.target)) {
+          this.setState({edited:false});
+      }
+  }
+  render() {
+    let { date,
+      id,
+      currentUser,
+      profileImageUrl,
+      text,
+      username,
+      removeMessage,
+      updateMessage,
+      isCorrectUser } = this.props;
+
+    let theDisplayedText = <p>{text}</p>
+    if (this.state.edited && isCorrectUser) {
+      theDisplayedText = <p><input type="text" value={this.state.text} name="text" onChange={this.handleChange}></input></p>
+    }
+    return (
+      <div onDoubleClick={() => this.setState({ edited: true })}  ref={this.wrapperRef}>
+        <li className="list-group-item">
+          <img
+            src={profileImageUrl || DefaultProfileImg}
+            alt={username}
+            height="100"
+            width="100"
+            className="timeline-image"
+          />
+          <div className="message-area" >
+            <Link to="/">@{username} &nbsp;</Link>
+            <span className="text-muted">
+              <Moment className="text-muted" format="Do MMM YYYY">
+                {date}
+              </Moment>
+            </span>
+            {theDisplayedText}
+            {isCorrectUser && (
+              <div>
+                <a className="btn btn-danger" onClick={removeMessage}>
+                  Delete
+         </a>
+                <button className="btn btn-success" >
+                  update
+         </button>
+              </div>
+            )}
+          </div>
+        </li>
+      </div>)
+  }
+
+}
