3379bb5b30b4791a5c9c9e92e561a20d33cb54bb
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-08-22 23:29:55 +0200

    frontend fixes

---
+++ src/store/actions/users.js
@@ -0,0 +1,22 @@
+import { apiCall } from "../../services/api";
+import { addError } from "./errors";
+import { LOAD_USERS } from "../actionTypes";
+
+
+export const loadUsers = users => ({
+    type: LOAD_USERS,
+    users
+  });
+
+export const fetchUsers = () => {
+    console.log('hi');
+    return dispatch => {
+      return apiCall("get", "/api/users")
+        .then(res => {
+          dispatch(loadUsers(res));
+        })
+        .catch(err => {
+          dispatch(addError(err.message));
+        });
+    };
+  };
