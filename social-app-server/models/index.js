32c00bb1d71f25fdc8b6efbe7042ccb557ded4a0
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-11-05 12:34:19 +0100

    ready for heroku

--- social-app-server/models/index.js
+++ social-app-server/models/index.js
@@ -1,8 +1,6 @@
 const mongoose =require('mongoose');
 mongoose.set('debug',true);
 mongoose.Promise=Promise;
-mongoose.connect('mongodb://localhost/social-app',{
-    keepAlive:true 
-})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-app');
 module.exports.User=require('./user')
 module.exports.Message=require('./message')
